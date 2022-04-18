a7ecccf8ae80651153d79956b877bc5ea536a6cf
imHurad <99075547+imHurad@users.noreply.github.com>
2022-04-18 15:58:02 +0430

    Update mirror.py

--- bot/modules/mirror.py
+++ bot/modules/mirror.py
@@ -151,7 +151,7 @@ class MirrorListener:
                         osremove(f_path)
         if self.isLeech:
             size = get_path_size(f'{DOWNLOAD_DIR}{self.uid}')
-            LOGGER.info(f"Leech Name: {up_name}")
+            LOGGER.info(f"╔—●Leech Name: {up_name}")
             tg = TgUploader(up_name, self)
             tg_upload_status = TgUploadStatus(tg, size, gid, self)
             with download_dict_lock:
@@ -160,7 +160,7 @@ class MirrorListener:
             tg.upload()
         else:
             size = get_path_size(up_path)
-            LOGGER.info(f"Upload Name: {up_name}")
+            LOGGER.info(f"╔—●Upload Name: {up_name}")
             drive = GoogleDriveHelper(up_name, self)
             upload_status = UploadStatus(drive, size, gid, self)
             with download_dict_lock:
@@ -186,13 +186,13 @@ class MirrorListener:
             update_all_messages()
 
     def onUploadComplete(self, link: str, size, files, folders, typ, name: str):
-        msg = f'<b>╔—●ɴᴀᴍᴇ: </b><code>{escape(name)}</code>\n\n<b>╟—●ꜱɪᴢᴇ:  </b>{size}'
+        msg = f'<b>╔—●ɴᴀᴍᴇ: </b><code>{escape(name)}</code>\n<b>╟—●🖇ꜱɪᴢᴇ: </b>{size}'
         if self.isLeech:
             count = len(files)
-            msg += f'\n<b>╟—●ᴛʏᴘᴇ: </b>{count}'
+            msg += f'\n<b>╟—●Total Files: </b>{count}'
             if typ != 0:
-                msg += f'\n<b>╟—●ꜱᴜʙꜰᴏʟᴅᴇʀꜱ: </b>{typ}'
-            msg += f'\n<b>╚—●ᴄᴄ:  </b>{self.tag}\n\n'
+                msg += f'\n<b>╟—●Corrupted Files: </b>{typ}'
+            msg += f'\n<b>╚—●cc: </b>{self.tag}\n'
             if self.message.chat.type == 'private':
                 sendMessage(msg, self.bot, self.message)
             else:
@@ -220,11 +220,11 @@ class MirrorListener:
             else:
                 update_all_messages()
         else:
-            msg += f'\n<b>╟—●ᴛʏᴘᴇ: </b>{typ}'
+            msg += f'\n\n<b>╟—●Type: </b>{typ}'
             if ospath.isdir(f'{DOWNLOAD_DIR}{self.uid}/{name}'):
-                msg += f'\n<b>╟—●ᴛʏᴘᴇ: </b>{folders}'
-                msg += f'\n<b>╟—●ꜰɪʟᴇꜱ: </b>{files}'
-            msg += f'\n<b>╚—●ᴄᴄ:  </b>{self.tag}'
+                msg += f'\n<b>╟—●SubFolders: </b>{folders}'
+                msg += f'\n<b>╟—●Files: </b>{files}'
+            msg += f'\n\n<b>╚—●cc: </b>{self.tag}'
             buttons = ButtonMaker()
             link = short_url(link)
             buttons.buildbutton("☁️ Drive Link", link)
