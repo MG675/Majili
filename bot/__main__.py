bdff67342f470c6edcc17bfcd4318cddd3edb888
imHurad <99075547+imHurad@users.noreply.github.com>
2022-04-18 23:06:30 +0430

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -1,5 +1,4 @@
 from signal import signal, SIGINT
-
 from os import path as ospath, remove as osremove, execl as osexecl
 from subprocess import run as srun, check_output
 from psutil import disk_usage, cpu_percent, swap_memory, cpu_count, virtual_memory, net_io_counters, boot_time
@@ -65,7 +64,8 @@ def stats(update, context):
 
 def start(update, context):
     buttons = ButtonMaker()
-    buttons.buildbutton("Owner", "https://t.me/imHurad")
+    buttons.buildbutton("Repo", "https://t.me/imhurad")
+    buttons.buildbutton("Report Group", "https://t.me/imhurad")
     reply_markup = InlineKeyboardMarkup(buttons.build_menu(2))
     if CustomFilters.authorized_user(update) or CustomFilters.authorized_chat(update):
         start_string = f'''
@@ -74,7 +74,7 @@ Type /{BotCommands.HelpCommand} to get a list of available commands
 '''
         sendMarkup(start_string, context.bot, update.message, reply_markup)
     else:
-        sendMarkup('Not Authorized user', context.bot, update.message, reply_markup)
+        sendMarkup('Not Authorized user, deploy your own mirror-leech bot', context.bot, update.message, reply_markup)
 
 def restart(update, context):
     restart_message = sendMessage("Restarting...", context.bot, update.message)
