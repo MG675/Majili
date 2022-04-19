d17807ede04219504e95bc3f08e17c2991c075eb
imHurad <99075547+imHurad@users.noreply.github.com>
2022-04-19 18:16:50 +0430

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -69,15 +69,15 @@ def start(update, context):
     reply_markup = InlineKeyboardMarkup(buttons.build_menu(2))
     if CustomFilters.authorized_user(update) or CustomFilters.authorized_chat(update):
         start_string = f'''
-ᴛʜɪꜱ ʙᴏᴛ ᴄᴀɴ ᴍɪʀʀᴏʀ ᴀʟʟ ʏᴏᴜʀ ʟɪɴᴋꜱ ᴛᴏ ɢᴏᴏɢʟᴇ ᴅʀɪᴠᴇ!
-ᴛʏᴘᴇ /{BotCommands.HelpCommand} ᴛᴏ ɢᴇᴛ ᴀ ʟɪꜱᴛ ᴏꜰ ᴀᴠᴀɪʟᴀʙʟᴇ ᴄᴏᴍᴍᴀɴᴅꜱ
+This bot can mirror all your links to Google Drive!
+Type /{BotCommands.HelpCommand} to get a list of available commands
 '''
         sendMarkup(start_string, context.bot, update.message, reply_markup)
     else:
-        sendMarkup('ɴᴏᴛ ᴀᴜᴛʜᴏʀɪᴢᴇᴅ ᴜꜱᴇʀ, ᴅᴇᴘʟᴏʏ ʏᴏᴜʀ ᴏᴡɴ ᴍɪʀʀᴏʀ-ʟᴇᴇᴄʜ ʙᴏᴛ', context.bot, update.message, reply_markup)
+        sendMarkup('Not Authorized user, deploy your own mirror-leech bot', context.bot, update.message, reply_markup)
 
 def restart(update, context):
-    restart_message = sendMessage("ʀᴇꜱᴛᴀʀᴛɪɴɢ...", context.bot, update.message)
+    restart_message = sendMessage("Restarting...", context.bot, update.message)
     if Interval:
         Interval[0].cancel()
     alive.kill()
@@ -93,9 +93,9 @@ def restart(update, context):
 
 def ping(update, context):
     start_time = int(round(time() * 1000))
-    reply = sendMessage("ꜱᴛᴀʀᴛɪɴɢ ᴘɪɴɢ", context.bot, update.message)
+    reply = sendMessage("Starting Ping", context.bot, update.message)
     end_time = int(round(time() * 1000))
-    editMessage(f'{end_time - start_time} ᴍꜱ', reply)
+    editMessage(f'{end_time - start_time} ms', reply)
 
 
 def log(update, context):
@@ -103,127 +103,138 @@ def log(update, context):
 
 
 help_string_telegraph = f'''<br>
-<b>/{BotCommands.HelpCommand}</b>: ᴛᴏ ɢᴇᴛ ᴛʜɪꜱ ᴍᴇꜱꜱᴀɢᴇ
+<b>/{BotCommands.HelpCommand}</b>: To get this message
 <br><br>
-<b>/{BotCommands.MirrorCommand}</b> [download_url][magnet_link]: ꜱᴛᴀʀᴛ ᴍɪʀʀᴏʀɪɴɢ ᴛᴏ ɢᴏᴏɢʟᴇ ᴅʀɪᴠᴇ. ꜱᴇɴᴅ <b>/{BotCommands.MirrorCommand}</b> ꜰᴏʀ ᴍᴏʀᴇ ʜᴇʟᴘ
+<b>/{BotCommands.MirrorCommand}</b> [download_url][magnet_link]: Start mirroring to Google Drive. Send <b>/{BotCommands.MirrorCommand}</b> for more help
 <br><br>
-<b>/{BotCommands.ZipMirrorCommand}</b> [download_url][magnet_link]: ꜱᴛᴀʀᴛ ᴍɪʀʀᴏʀɪɴɢ ᴀɴᴅ ᴜᴘʟᴏᴀᴅ ᴛʜᴇ ꜰɪʟᴇ/ꜰᴏʟᴅᴇʀ ᴇxᴛʀᴀᴄᴛᴇᴅ ꜰʀᴏᴍ ᴀɴʏ ᴀʀᴄʜɪᴠᴇ ᴇxᴛᴇɴꜱɪᴏɴ
+<b>/{BotCommands.ZipMirrorCommand}</b> [download_url][magnet_link]: Start mirroring and upload the file/folder compressed with zip extension
 <br><br>
-<b>/{BotCommands.QbMirrorCommand}</b> [magnet_link][torrent_file][torrent_file_url]: ꜱᴛᴀʀᴛ ᴍɪʀʀᴏʀɪɴɢ ᴜꜱɪɴɢ Qʙɪᴛᴛᴏʀʀᴇɴᴛ, ᴜꜱᴇ <b>/{BotCommands.QbMirrorCommand} s</b> ᴛᴏ ꜱᴇʟᴇᴄᴛ ꜰɪʟᴇꜱ ʙᴇꜰᴏʀᴇ ᴅᴏᴡɴʟᴏᴀᴅɪɴɢ
+<b>/{BotCommands.UnzipMirrorCommand}</b> [download_url][magnet_link]: Start mirroring and upload the file/folder extracted from any archive extension
 <br><br>
-<b>/{BotCommands.QbZipMirrorCommand}</b> [magnet_link][torrent_file][torrent_file_url]: ꜱᴛᴀʀᴛ ᴍɪʀʀᴏʀɪɴɢ ᴜꜱɪɴɢ Qʙɪᴛᴛᴏʀʀᴇɴᴛ ᴀɴᴅ ᴜᴘʟᴏᴀᴅ ᴛʜᴇ ꜰɪʟᴇ/ꜰᴏʟᴅᴇʀ ᴄᴏᴍᴘʀᴇꜱꜱᴇᴅ ᴡɪᴛʜ ᴢɪᴘ ᴇxᴛᴇɴꜱɪᴏɴ
+<b>/{BotCommands.QbMirrorCommand}</b> [magnet_link][torrent_file][torrent_file_url]: Start Mirroring using qBittorrent, Use <b>/{BotCommands.QbMirrorCommand} s</b> to select files before downloading
 <br><br>
-<b>/{BotCommands.QbUnzipMirrorCommand}</b> [magnet_link][torrent_file][torrent_file_url]: ꜱᴛᴀʀᴛ ᴍɪʀʀᴏʀɪɴɢ ᴜꜱɪɴɢ Qʙɪᴛᴛᴏʀʀᴇɴᴛ ᴀɴᴅ ᴜᴘʟᴏᴀᴅ ᴛʜᴇ ꜰɪʟᴇ/ꜰᴏʟᴅᴇʀ ᴇxᴛʀᴀᴄᴛᴇᴅ ꜰʀᴏᴍ ᴀɴʏ ᴀʀᴄʜɪᴠᴇ ᴇxᴛᴇɴꜱɪᴏɴ
+<b>/{BotCommands.QbZipMirrorCommand}</b> [magnet_link][torrent_file][torrent_file_url]: Start mirroring using qBittorrent and upload the file/folder compressed with zip extension
 <br><br>
-<b>/{BotCommands.LeechCommand}</b> [download_url][magnet_link]: ꜱᴛᴀʀᴛ ʟᴇᴇᴄʜɪɴɢ ᴛᴏ ᴛᴇʟᴇɢʀᴀᴍ, ᴜꜱᴇ <b>/{BotCommands.LeechCommand} s</b> ᴛᴏ ꜱᴇʟᴇᴄᴛ ꜰɪʟᴇꜱ ʙᴇꜰᴏʀᴇ ʟᴇᴇᴄʜɪɴɢ
+<b>/{BotCommands.QbUnzipMirrorCommand}</b> [magnet_link][torrent_file][torrent_file_url]: Start mirroring using qBittorrent and upload the file/folder extracted from any archive extension
 <br><br>
-<b>/{BotCommands.ZipLeechCommand}</b> [download_url][magnet_link]: ꜱᴛᴀʀᴛ ʟᴇᴇᴄʜɪɴɢ ᴛᴏ ᴛᴇʟᴇɢʀᴀᴍ ᴀɴᴅ ᴜᴘʟᴏᴀᴅ ᴛʜᴇ ꜰɪʟᴇ/ꜰᴏʟᴅᴇʀ ᴄᴏᴍᴘʀᴇꜱꜱᴇᴅ ᴡɪᴛʜ ᴢɪᴘ ᴇxᴛᴇɴꜱɪᴏɴ
+<b>/{BotCommands.LeechCommand}</b> [download_url][magnet_link]: Start leeching to Telegram, Use <b>/{BotCommands.LeechCommand} s</b> to select files before leeching
 <br><br>
-<b>/{BotCommands.UnzipLeechCommand}</b> [download_url][magnet_link][torent_file]: ꜱᴛᴀʀᴛ ʟᴇᴇᴄʜɪɴɢ ᴛᴏ ᴛᴇʟᴇɢʀᴀᴍ ᴀɴᴅ ᴜᴘʟᴏᴀᴅ ᴛʜᴇ ꜰɪʟᴇ/ꜰᴏʟᴅᴇʀ ᴇxᴛʀᴀᴄᴛᴇᴅ ꜰʀᴏᴍ ᴀɴʏ ᴀʀᴄʜɪᴠᴇ ᴇxᴛᴇɴꜱɪᴏɴ
+<b>/{BotCommands.ZipLeechCommand}</b> [download_url][magnet_link]: Start leeching to Telegram and upload the file/folder compressed with zip extension
 <br><br>
-<b>/{BotCommands.QbLeechCommand}</b> [magnet_link][torrent_file][torrent_file_url]: ꜱᴛᴀʀᴛ ʟᴇᴇᴄʜɪɴɢ ᴛᴏ ᴛᴇʟᴇɢʀᴀᴍ ᴜꜱɪɴɢ Qʙɪᴛᴛᴏʀʀᴇɴᴛ, ᴜꜱᴇ <b>/{BotCommands.QbLeechCommand} s</b> ᴛᴏ ꜱᴇʟᴇᴄᴛ ꜰɪʟᴇꜱ ʙᴇꜰᴏʀᴇ ʟᴇᴇᴄʜɪɴɢ
+<b>/{BotCommands.UnzipLeechCommand}</b> [download_url][magnet_link][torent_file]: Start leeching to Telegram and upload the file/folder extracted from any archive extension
 <br><br>
-<b>/{BotCommands.QbZipLeechCommand}</b> [magnet_link][torrent_file][torrent_file_url]: ꜱᴛᴀʀᴛ ʟᴇᴇᴄʜɪɴɢ ᴛᴏ ᴛᴇʟᴇɢʀᴀᴍ ᴜꜱɪɴɢ Qʙɪᴛᴛᴏʀʀᴇɴᴛ ᴀɴᴅ ᴜᴘʟᴏᴀᴅ ᴛʜᴇ ꜰɪʟᴇ/ꜰᴏʟᴅᴇʀ ᴄᴏᴍᴘʀᴇꜱꜱᴇᴅ ᴡɪᴛʜ ᴢɪᴘ ᴇxᴛᴇɴꜱɪᴏɴ
+<b>/{BotCommands.QbLeechCommand}</b> [magnet_link][torrent_file][torrent_file_url]: Start leeching to Telegram using qBittorrent, Use <b>/{BotCommands.QbLeechCommand} s</b> to select files before leeching
 <br><br>
-<b>/{BotCommands.QbUnzipLeechCommand}</b> [magnet_link][torrent_file][torrent_file_url]: ꜱᴛᴀʀᴛ ʟᴇᴇᴄʜɪɴɢ ᴛᴏ ᴛᴇʟᴇɢʀᴀᴍ ᴜꜱɪɴɢ Qʙɪᴛᴛᴏʀʀᴇɴᴛ ᴀɴᴅ ᴜᴘʟᴏᴀᴅ ᴛʜᴇ ꜰɪʟᴇ/ꜰᴏʟᴅᴇʀ ᴇxᴛʀᴀᴄᴛᴇᴅ ꜰʀᴏᴍ ᴀɴʏ ᴀʀᴄʜɪᴠᴇ ᴇxᴛᴇɴꜱɪᴏɴ
+<b>/{BotCommands.QbZipLeechCommand}</b> [magnet_link][torrent_file][torrent_file_url]: Start leeching to Telegram using qBittorrent and upload the file/folder compressed with zip extension
 <br><br>
-<b>/{BotCommands.CloneCommand}</b> [drive_url][gdtot_url]: ᴄᴏᴘʏ ꜰɪʟᴇ/ꜰᴏʟᴅᴇʀ ᴛᴏ ɢᴏᴏɢʟᴇ ᴅʀɪᴠᴇ
+<b>/{BotCommands.QbUnzipLeechCommand}</b> [magnet_link][torrent_file][torrent_file_url]: Start leeching to Telegram using qBittorrent and upload the file/folder extracted from any archive extension
 <br><br>
-<b>/{BotCommands.CountCommand}</b> [drive_url][gdtot_url]: ᴄᴏᴜɴᴛ ꜰɪʟᴇ/ꜰᴏʟᴅᴇʀ ᴏꜰ ɢᴏᴏɢʟᴇ ᴅʀɪᴠᴇ
+<b>/{BotCommands.CloneCommand}</b> [drive_url][gdtot_url]: Copy file/folder to Google Drive
 <br><br>
-<b>/{BotCommands.DeleteCommand}</b> [drive_url]: ᴅᴇʟᴇᴛᴇ ꜰɪʟᴇ/ꜰᴏʟᴅᴇʀ ꜰʀᴏᴍ ɢᴏᴏɢʟᴇ ᴅʀɪᴠᴇ (ᴏɴʟʏ ᴏᴡɴᴇʀ & ꜱᴜᴅᴏ)
+<b>/{BotCommands.CountCommand}</b> [drive_url][gdtot_url]: Count file/folder of Google Drive
 <br><br>
-<b>/{BotCommands.WatchCommand}</b> [yt-dlp supported link]: ᴍɪʀʀᴏʀ ʏᴛ-ᴅʟᴘ ꜱᴜᴘᴘᴏʀᴛᴇᴅ ʟɪɴᴋ. ꜱᴇɴᴅ <b>/{BotCommands.WatchCommand}</b> ꜰᴏʀ ᴍᴏʀᴇ ʜᴇʟᴘ
+<b>/{BotCommands.DeleteCommand}</b> [drive_url]: Delete file/folder from Google Drive (Only Owner & Sudo)
 <br><br>
-<b>/{BotCommands.ZipWatchCommand}</b> [yt-dlp supported link]: ᴍɪʀʀᴏʀ ʏᴛ-ᴅʟᴘ ꜱᴜᴘᴘᴏʀᴛᴇᴅ ʟɪɴᴋ ᴀꜱ ᴢɪᴘ
+<b>/{BotCommands.WatchCommand}</b> [yt-dlp supported link]: Mirror yt-dlp supported link. Send <b>/{BotCommands.WatchCommand}</b> for more help
 <br><br>
-<b>/{BotCommands.LeechWatchCommand}</b> [yt-dlp supported link]: ʟᴇᴇᴄʜ ʏᴛ-ᴅʟᴘ ꜱᴜᴘᴘᴏʀᴛᴇᴅ ʟɪɴᴋ
+<b>/{BotCommands.ZipWatchCommand}</b> [yt-dlp supported link]: Mirror yt-dlp supported link as zip
 <br><br>
-<b>/{BotCommands.LeechZipWatchCommand}</b> [yt-dlp supported link]: ʟᴇᴇᴄʜ ʏᴛ-ᴅʟᴘ ꜱᴜᴘᴘᴏʀᴛᴇᴅ ʟɪɴᴋ ᴀꜱ ᴢɪᴘ
+<b>/{BotCommands.LeechWatchCommand}</b> [yt-dlp supported link]: Leech yt-dlp supported link
 <br><br>
-<b>/{BotCommands.LeechSetCommand}</b>: ʟᴇᴇᴄʜ ꜱᴇᴛᴛɪɴɢꜱ
+<b>/{BotCommands.LeechZipWatchCommand}</b> [yt-dlp supported link]: Leech yt-dlp supported link as zip
 <br><br>
-<b>/{BotCommands.SetThumbCommand}</b>: ʀᴇᴘʟʏ ᴘʜᴏᴛᴏ ᴛᴏ ꜱᴇᴛ ɪᴛ ᴀꜱ ᴛʜᴜᴍʙɴᴀɪʟ
+<b>/{BotCommands.LeechSetCommand}</b>: Leech settings
 <br><br>
-<b>/{BotCommands.RssListCommand}</b>: ʟɪꜱᴛ ᴀʟʟ ꜱᴜʙꜱᴄʀɪʙᴇᴅ ʀꜱꜱ ꜰᴇᴇᴅ ɪɴꜰᴏo
+<b>/{BotCommands.SetThumbCommand}</b>: Reply photo to set it as Thumbnail
 <br><br>
-<b>/{BotCommands.RssGetCommand}</b>: [Title] [Number](last N links): ꜰᴏʀᴄᴇ ꜰᴇᴛᴄʜ ʟᴀꜱᴛ ɴ ʟɪɴᴋꜱ
+<b>/{BotCommands.RssListCommand}</b>: List all subscribed rss feed info
 <br><br>
-<b>/{BotCommands.RssSubCommand}</b>: [Title] [Rss Link] f: [filter]: ꜱᴜʙꜱᴄʀɪʙᴇ ɴᴇᴡ ʀꜱꜱ ꜰᴇᴇᴅ
+<b>/{BotCommands.RssGetCommand}</b>: [Title] [Number](last N links): Force fetch last N links
 <br><br>
-<b>/{BotCommands.RssUnSubCommand}</b>: [Title]: ᴜɴᴜʙꜱᴄʀɪʙᴇ ʀꜱꜱ ꜰᴇᴇᴅ ʙʏ ᴛɪᴛʟᴇ
+<b>/{BotCommands.RssSubCommand}</b>: [Title] [Rss Link] f: [filter]: Subscribe new rss feed
 <br><br>
-<b>/{BotCommands.RssSettingsCommand}</b>: ʀꜱꜱ ꜱᴇᴛᴛɪɴɢꜱ
+<b>/{BotCommands.RssUnSubCommand}</b>: [Title]: Unubscribe rss feed by title
 <br><br>
-<b>/{BotCommands.CancelMirror}</b>: ʀᴇᴘʟʏ ᴛᴏ ᴛʜᴇ ᴍᴇꜱꜱᴀɢᴇ ʙʏ ᴡʜɪᴄʜ ᴛʜᴇ ᴅᴏᴡɴʟᴏᴀᴅ ᴡᴀꜱ ɪɴɪᴛɪᴀᴛᴇᴅ ᴀɴᴅ ᴛʜᴀᴛ ᴅᴏᴡɴʟᴏᴀᴅ ᴡɪʟʟ ʙᴇ ᴄᴀɴᴄᴇʟʟᴇᴅ
+<b>/{BotCommands.RssSettingsCommand}</b>: Rss Settings
 <br><br>
-<b>/{BotCommands.CancelAllCommand}</b>: ᴄᴀɴᴄᴇʟ ᴀʟʟ ᴅᴏᴡɴʟᴏᴀᴅɪɴɢ ᴛᴀꜱᴋꜱ
+<b>/{BotCommands.CancelMirror}</b>: Reply to the message by which the download was initiated and that download will be cancelled
 <br><br>
-<b>/{BotCommands.ListCommand}</b> [query]: ꜱᴇᴀʀᴄʜ ɪɴ ɢᴏᴏɢʟᴇ ᴅʀɪᴠᴇ(ꜱ)
+<b>/{BotCommands.CancelAllCommand}</b>: Cancel all downloading tasks
 <br><br>
-<b>/{BotCommands.SearchCommand}</b> [query]: ꜱᴇᴀʀᴄʜ ꜰᴏʀ ᴛᴏʀʀᴇɴᴛꜱ ᴡɪᴛʜ ᴀᴘɪ
-<ʙʀ>ꜱɪᴛᴇꜱ: <ᴄᴏᴅᴇ>ʀᴀʀʙɢ, 1337x, ʏᴛꜱ, ᴇᴛᴢᴠ, ᴛɢx, ᴛᴏʀʟᴏᴄᴋ, ᴘɪʀᴀᴛᴇʙᴀʏ, ɴʏᴀᴀꜱɪ, ᴇᴛᴛᴠ<ʙʀ><ʙʀ>
-<b>/{BotCommands.StatusCommand}</b>: ꜱʜᴏᴡꜱ ᴀ ꜱᴛᴀᴛᴜꜱ ᴏꜰ ᴀʟʟ ᴛʜᴇ ᴅᴏᴡɴʟᴏᴀᴅꜱ
+<b>/{BotCommands.ListCommand}</b> [query]: Search in Google Drive(s)
 <br><br>
-<b>/{BotCommands.StatsCommand}</b>: ꜱʜᴏᴡ ꜱᴛᴀᴛꜱ ᴏꜰ ᴛʜᴇ ᴍᴀᴄʜɪɴᴇ ᴛʜᴇ ʙᴏᴛ ɪꜱ ʜᴏꜱᴛᴇᴅ ᴏɴ
+<b>/{BotCommands.SearchCommand}</b> [query]: Search for torrents with API
+<br>sites: <code>rarbg, 1337x, yts, etzv, tgx, torlock, piratebay, nyaasi, ettv</code><br><br>
+<b>/{BotCommands.StatusCommand}</b>: Shows a status of all the downloads
+<br><br>
+<b>/{BotCommands.StatsCommand}</b>: Show Stats of the machine the bot is hosted on
 '''
 
 help = telegraph.create_page(
-        title='ᴍɪʀʀᴏʀ-ʟᴇᴇᴄʜ-ʙᴏᴛ ʜᴇʟᴘ',
+        title='Mirror-Leech-Bot Help',
         content=help_string_telegraph,
     )["path"]
 
 help_string = f'''
-/{BotCommands.PingCommand}: ᴄʜᴇᴄᴋ ʜᴏᴡ ʟᴏɴɢ ɪᴛ ᴛᴀᴋᴇꜱ ᴛᴏ ᴘɪɴɢ ᴛʜᴇ ʙᴏᴛ
-/{BotCommands.AuthorizeCommand}: ᴀᴜᴛʜᴏʀɪᴢᴇ ᴀ ᴄʜᴀᴛ ᴏʀ ᴀ ᴜꜱᴇʀ ᴛᴏ ᴜꜱᴇ ᴛʜᴇ ʙᴏᴛ (ᴄᴀɴ ᴏɴʟʏ ʙᴇ ɪɴᴠᴏᴋᴇᴅ ʙʏ ᴏᴡɴᴇʀ & ꜱᴜᴅᴏ ᴏꜰ ᴛʜᴇ ʙᴏᴛ)
-/{BotCommands.UnAuthorizeCommand}:ᴜɴᴀᴜᴛʜᴏʀɪᴢᴇ ᴀ ᴄʜᴀᴛ ᴏʀ ᴀ ᴜꜱᴇʀ ᴛᴏ ᴜꜱᴇ ᴛʜᴇ ʙᴏᴛ (ᴄᴀɴ ᴏɴʟʏ ʙᴇ ɪɴᴠᴏᴋᴇᴅ ʙʏ ᴏᴡɴᴇʀ & ꜱᴜᴅᴏ ᴏꜰ ᴛʜᴇ ʙᴏᴛ)
-/{BotCommands.AuthorizedUsersCommand}: ꜱʜᴏᴡ ᴀᴜᴛʜᴏʀɪᴢᴇᴅ ᴜꜱᴇʀꜱ (ᴏɴʟʏ ᴏᴡɴᴇʀ & ꜱᴜᴅᴏ)
-/{BotCommands.AddSudoCommand}: ᴀᴅᴅ ꜱᴜᴅᴏ ᴜꜱᴇʀ (ᴏɴʟʏ ᴏᴡɴᴇʀ)
-/{BotCommands.RmSudoCommand}: ʀᴇᴍᴏᴠᴇ ꜱᴜᴅᴏ ᴜꜱᴇʀꜱ (ᴏɴʟʏ ᴏᴡɴᴇʀ)
-/{BotCommands.RestartCommand}: ʀᴇꜱᴛᴀʀᴛ ᴀɴᴅ ᴜᴘᴅᴀᴛᴇ ᴛʜᴇ ʙᴏᴛ
-/{BotCommands.LogCommand}: ɢᴇᴛ ᴀ ʟᴏɢ ꜰɪʟᴇ ᴏꜰ ᴛʜᴇ ʙᴏᴛ. ʜᴀɴᴅʏ ꜰᴏʀ ɢᴇᴛᴛɪɴɢ ᴄʀᴀꜱʜ ʀᴇᴘᴏʀᴛꜱ
-/{BotCommands.ShellCommand}: ʀᴜɴ ᴄᴏᴍᴍᴀɴᴅꜱ ɪɴ ꜱʜᴇʟʟ (ᴏɴʟʏ ᴏᴡɴᴇʀ)
-/{BotCommands.ExecHelpCommand}: ɢᴇᴛ ʜᴇʟᴘ ꜰᴏʀ ᴇxᴇᴄᴜᴛᴏʀ ᴍᴏᴅᴜʟᴇ (ᴏɴʟʏ ᴏᴡɴᴇʀ)
+/{BotCommands.PingCommand}: Check how long it takes to Ping the Bot
+
+/{BotCommands.AuthorizeCommand}: Authorize a chat or a user to use the bot (Can only be invoked by Owner & Sudo of the bot)
+
+/{BotCommands.UnAuthorizeCommand}: Unauthorize a chat or a user to use the bot (Can only be invoked by Owner & Sudo of the bot)
+
+/{BotCommands.AuthorizedUsersCommand}: Show authorized users (Only Owner & Sudo)
+
+/{BotCommands.AddSudoCommand}: Add sudo user (Only Owner)
+
+/{BotCommands.RmSudoCommand}: Remove sudo users (Only Owner)
+
+/{BotCommands.RestartCommand}: Restart and update the bot
+
+/{BotCommands.LogCommand}: Get a log file of the bot. Handy for getting crash reports
+
+/{BotCommands.ShellCommand}: Run commands in Shell (Only Owner)
+
+/{BotCommands.ExecHelpCommand}: Get help for Executor module (Only Owner)
 '''
 
 def bot_help(update, context):
     button = ButtonMaker()
-    button.buildbutton("ᴏᴛʜᴇʀ ᴄᴏᴍᴍᴀɴᴅꜱ", f"https://telegra.ph/{help}")
+    button.buildbutton("Other Commands", f"https://telegra.ph/{help}")
     reply_markup = InlineKeyboardMarkup(button.build_menu(1))
     sendMarkup(help_string, context.bot, update.message, reply_markup)
 
 botcmds = [
 
-        (f'{BotCommands.MirrorCommand}', 'ᴍɪʀʀᴏʀ'),
-        (f'{BotCommands.ZipMirrorCommand}','ᴍɪʀʀᴏʀ ᴀɴᴅ ᴜᴘʟᴏᴀᴅ ᴀꜱ ᴢɪᴘ'),
-        (f'{BotCommands.UnzipMirrorCommand}','ᴍɪʀʀᴏʀ ᴀɴᴅ ᴇxᴛʀᴀᴄᴛ ꜰɪʟᴇꜱ'),
-        (f'{BotCommands.QbMirrorCommand}','ᴍɪʀʀᴏʀ ᴛᴏʀʀᴇɴᴛ ᴜꜱɪɴɢ Qʙɪᴛᴛᴏʀʀᴇɴᴛ'),
-        (f'{BotCommands.QbZipMirrorCommand}','ᴍɪʀʀᴏʀ ᴛᴏʀʀᴇɴᴛ ᴀɴᴅ ᴜᴘʟᴏᴀᴅ ᴀꜱ ᴢɪᴘ ᴜꜱɪɴɢ Qʙ'),
-        (f'{BotCommands.QbUnzipMirrorCommand}','ᴍɪʀʀᴏʀ ᴛᴏʀʀᴇɴᴛ ᴀɴᴅ ᴇxᴛʀᴀᴄᴛ ꜰɪʟᴇꜱ ᴜꜱɪɴɢ Qʙ'),
-        (f'{BotCommands.WatchCommand}','ᴍɪʀʀᴏʀ ʏᴛ-ᴅʟᴘ ꜱᴜᴘᴘᴏʀᴛᴇᴅ ʟɪɴᴋ'),
-        (f'{BotCommands.ZipWatchCommand}','ᴍɪʀʀᴏʀ ʏᴛ-ᴅʟᴘ ꜱᴜᴘᴘᴏʀᴛᴇᴅ ʟɪɴᴋ ᴀꜱ ᴢɪᴘ'),
-        (f'{BotCommands.CloneCommand}','ᴄᴏᴘʏ ꜰɪʟᴇ/ꜰᴏʟᴅᴇʀ ᴛᴏ ᴅʀɪᴠᴇ'),
-        (f'{BotCommands.LeechCommand}','ʟᴇᴇᴄʜ'),
-        (f'{BotCommands.ZipLeechCommand}','ʟᴇᴇᴄʜ ᴀɴᴅ ᴜᴘʟᴏᴀᴅ ᴀꜱ ᴢɪᴘ'),
-        (f'{BotCommands.UnzipLeechCommand}','ʟᴇᴇᴄʜ ᴀɴᴅ ᴇxᴛʀᴀᴄᴛ ꜰɪʟᴇꜱ'),
-        (f'{BotCommands.QbLeechCommand}','ʟᴇᴇᴄʜ ᴛᴏʀʀᴇɴᴛ ᴜꜱɪɴɢ Qʙɪᴛᴛᴏʀʀᴇɴᴛ'),
-        (f'{BotCommands.QbZipLeechCommand}','ʟᴇᴇᴄʜ ᴛᴏʀʀᴇɴᴛ ᴀɴᴅ ᴜᴘʟᴏᴀᴅ ᴀꜱ ᴢɪᴘ ᴜꜱɪɴɢ Qʙ'),
-        (f'{BotCommands.QbUnzipLeechCommand}','ʟᴇᴇᴄʜ ᴛᴏʀʀᴇɴᴛ ᴀɴᴅ ᴇxᴛʀᴀᴄᴛ ᴜꜱɪɴɢ Qʙ'),
-        (f'{BotCommands.LeechWatchCommand}','ʟᴇᴇᴄʜ ʏᴛ-ᴅʟᴘ ꜱᴜᴘᴘᴏʀᴛᴇᴅ ʟɪɴᴋ'),
-        (f'{BotCommands.LeechZipWatchCommand}','ʟᴇᴇᴄʜ ʏᴛ-ᴅʟᴘ ꜱᴜᴘᴘᴏʀᴛᴇᴅ ʟɪɴᴋ ᴀꜱ ᴢɪᴘ'),
-        (f'{BotCommands.CountCommand}','ᴄᴏᴜɴᴛ ꜰɪʟᴇ/ꜰᴏʟᴅᴇʀ ᴏꜰ ᴅʀɪᴠᴇ'),
-        (f'{BotCommands.DeleteCommand}','ᴅᴇʟᴇᴛᴇ ꜰɪʟᴇ/ꜰᴏʟᴅᴇʀ ꜰʀᴏᴍ ᴅʀɪᴠᴇ'),
-        (f'{BotCommands.CancelMirror}','ᴄᴀɴᴄᴇʟ ᴀ ᴛᴀꜱᴋ'),
-        (f'{BotCommands.CancelAllCommand}','ᴄᴀɴᴄᴇʟ ᴀʟʟ ᴅᴏᴡɴʟᴏᴀᴅɪɴɢ ᴛᴀꜱᴋꜱ'),
-        (f'{BotCommands.ListCommand}','ꜱᴇᴀʀᴄʜ ɪɴ ᴅʀɪᴠᴇ'),
-        (f'{BotCommands.LeechSetCommand}','ʟᴇᴇᴄʜ ꜱᴇᴛᴛɪɴɢꜱ'),
-        (f'{BotCommands.SetThumbCommand}','ꜱᴇᴛ ᴛʜᴜᴍʙɴᴀɪʟ'),
-        (f'{BotCommands.StatusCommand}','ɢᴇᴛ ᴍɪʀʀᴏʀ ꜱᴛᴀᴛᴜꜱ ᴍᴇꜱꜱᴀɢᴇ'),
-        (f'{BotCommands.StatsCommand}','ʙᴏᴛ ᴜꜱᴀɢᴇ ꜱᴛᴀᴛꜱ'),
-        (f'{BotCommands.PingCommand}','ᴘɪɴɢ ᴛʜᴇ ʙᴏᴛ'),
-        (f'{BotCommands.RestartCommand}','ʀᴇꜱᴛᴀʀᴛ ᴛʜᴇ ʙᴏᴛ'),
-        (f'{BotCommands.LogCommand}','ɢᴇᴛ ᴛʜᴇ ʙᴏᴛ ʟᴏɢ'),
-        (f'{BotCommands.HelpCommand}','ɢᴇᴛ ᴅᴇᴛᴀɪʟᴇᴅ ʜᴇʟᴘ')
+        (f'{BotCommands.MirrorCommand}', 'Mirror'),
+        (f'{BotCommands.ZipMirrorCommand}','Mirror and upload as zip'),
+        (f'{BotCommands.UnzipMirrorCommand}','Mirror and extract files'),
+        (f'{BotCommands.QbMirrorCommand}','Mirror torrent using qBittorrent'),
+        (f'{BotCommands.QbZipMirrorCommand}','Mirror torrent and upload as zip using qb'),
+        (f'{BotCommands.QbUnzipMirrorCommand}','Mirror torrent and extract files using qb'),
+        (f'{BotCommands.WatchCommand}','Mirror yt-dlp supported link'),
+        (f'{BotCommands.ZipWatchCommand}','Mirror yt-dlp supported link as zip'),
+        (f'{BotCommands.CloneCommand}','Copy file/folder to Drive'),
+        (f'{BotCommands.LeechCommand}','Leech'),
+        (f'{BotCommands.ZipLeechCommand}','Leech and upload as zip'),
+        (f'{BotCommands.UnzipLeechCommand}','Leech and extract files'),
+        (f'{BotCommands.QbLeechCommand}','Leech torrent using qBittorrent'),
+        (f'{BotCommands.QbZipLeechCommand}','Leech torrent and upload as zip using qb'),
+        (f'{BotCommands.QbUnzipLeechCommand}','Leech torrent and extract using qb'),
+        (f'{BotCommands.LeechWatchCommand}','Leech yt-dlp supported link'),
+        (f'{BotCommands.LeechZipWatchCommand}','Leech yt-dlp supported link as zip'),
+        (f'{BotCommands.CountCommand}','Count file/folder of Drive'),
+        (f'{BotCommands.DeleteCommand}','Delete file/folder from Drive'),
+        (f'{BotCommands.CancelMirror}','Cancel a task'),
+        (f'{BotCommands.CancelAllCommand}','Cancel all downloading tasks'),
+        (f'{BotCommands.ListCommand}','Search in Drive'),
+        (f'{BotCommands.LeechSetCommand}','Leech settings'),
+        (f'{BotCommands.SetThumbCommand}','Set thumbnail'),
+        (f'{BotCommands.StatusCommand}','Get mirror status message'),
+        (f'{BotCommands.StatsCommand}','Bot usage stats'),
+        (f'{BotCommands.PingCommand}','Ping the bot'),
+        (f'{BotCommands.RestartCommand}','Restart the bot'),
+        (f'{BotCommands.LogCommand}','Get the bot Log'),
+        (f'{BotCommands.HelpCommand}','Get detailed help')
     ]
 
 def main():
@@ -233,13 +244,13 @@ def main():
     if ospath.isfile(".restartmsg"):
         with open(".restartmsg") as f:
             chat_id, msg_id = map(int, f)
-        bot.edit_message_text("ʀᴇꜱᴛᴀʀᴛᴇᴅ ꜱᴜᴄᴄᴇꜱꜱꜰᴜʟʟʏ!", chat_id, msg_id)
+        bot.edit_message_text("Restarted successfully!", chat_id, msg_id)
         osremove(".restartmsg")
     elif AUTHORIZED_CHATS:
         try:
             for i in AUTHORIZED_CHATS:
                 if str(i).startswith('-'):
-                    bot.sendMessage(chat_id=i, text="<b>ʙᴏᴛ ꜱᴛᴀʀᴛᴇᴅ!</b>", parse_mode=ParseMode.HTML)
+                    bot.sendMessage(chat_id=i, text="<b>Bot Started!</b>", parse_mode=ParseMode.HTML)
         except Exception as e:
             LOGGER.error(e)
 
@@ -260,7 +271,7 @@ def main():
     dispatcher.add_handler(stats_handler)
     dispatcher.add_handler(log_handler)
     updater.start_polling(drop_pending_updates=IGNORE_PENDING_REQUESTS)
-    LOGGER.info("ʙᴏᴛ ꜱᴛᴀʀᴛᴇᴅ!")
+    LOGGER.info("Bot Started!")
     signal(SIGINT, exit_clean_up)
     if rss_session is not None:
         rss_session.start()
