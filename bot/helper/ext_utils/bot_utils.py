905aa2df8f795e5738b26f453f40ec231f2f0f06
imHurad <99075547+imHurad@users.noreply.github.com>
2022-04-19 19:27:26 +0430

    Update bot_utils.py

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -126,7 +126,7 @@ def get_readable_message():
                 globals()['PAGE_NO'] -= 1
             START = COUNT
         for index, download in enumerate(list(download_dict.values())[START:], start=1):
-            msg += f"<b>╔—●ɴᴀᴍᴇ:</b> <code>{escape(str(download.name()))}</code>"
+            msg += f"<b>\n<╔—●ɴᴀᴍᴇ:</b> <code>{escape(str(download.name()))}</code>"
             msg += f"\n<b>╟—●ꜱᴛᴀᴛᴜꜱ:</b> <i>{download.status()}</i>"
             if download.status() not in [
                 MirrorStatus.STATUS_ARCHIVING,
